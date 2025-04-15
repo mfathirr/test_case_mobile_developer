c31c0faad414d49ae67c6b7b924978fa46eb6674
Oriadevid2025 <oriadevfigma@gmail.com>
2025-04-15 17:15:17 +0700

    ref(font) : change font from inter into plus jakarta sans

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,22 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  static TextTheme interTextTheme = TextTheme(
-    displayLarge: GoogleFonts.inter(fontSize: 57, fontWeight: FontWeight.bold),
-    displayMedium: GoogleFonts.inter(fontSize: 45, fontWeight: FontWeight.bold),
-    displaySmall: GoogleFonts.inter(fontSize: 36, fontWeight: FontWeight.bold),
-    headlineLarge: GoogleFonts.inter(fontSize: 32, fontWeight: FontWeight.w700),
+  static TextTheme jakartaSansTextTheme = TextTheme(
+    displayLarge:
+        GoogleFonts.plusJakartaSans(fontSize: 57, fontWeight: FontWeight.bold),
+    displayMedium:
+        GoogleFonts.plusJakartaSans(fontSize: 45, fontWeight: FontWeight.bold),
+    displaySmall:
+        GoogleFonts.plusJakartaSans(fontSize: 36, fontWeight: FontWeight.bold),
+    headlineLarge:
+        GoogleFonts.plusJakartaSans(fontSize: 32, fontWeight: FontWeight.w700),
     headlineMedium:
-        GoogleFonts.inter(fontSize: 28, fontWeight: FontWeight.w600),
-    headlineSmall: GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.w600),
-    titleLarge: GoogleFonts.inter(fontSize: 22, fontWeight: FontWeight.w500),
-    titleMedium: GoogleFonts.inter(fontSize: 16, fontWeight: FontWeight.w500),
-    titleSmall: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w500),
-    bodyLarge: GoogleFonts.inter(fontSize: 16, fontWeight: FontWeight.normal),
-    bodyMedium: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.normal),
-    bodySmall: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.normal),
-    labelLarge: GoogleFonts.inter(fontSize: 14, fontWeight: FontWeight.w500),
-    labelMedium: GoogleFonts.inter(fontSize: 12, fontWeight: FontWeight.w500),
-    labelSmall: GoogleFonts.inter(fontSize: 11, fontWeight: FontWeight.w500),
+        GoogleFonts.plusJakartaSans(fontSize: 28, fontWeight: FontWeight.w600),
+    headlineSmall:
+        GoogleFonts.plusJakartaSans(fontSize: 24, fontWeight: FontWeight.w600),
+    titleLarge:
+        GoogleFonts.plusJakartaSans(fontSize: 22, fontWeight: FontWeight.w500),
+    titleMedium:
+        GoogleFonts.plusJakartaSans(fontSize: 16, fontWeight: FontWeight.w500),
+    titleSmall:
+        GoogleFonts.plusJakartaSans(fontSize: 14, fontWeight: FontWeight.w500),
+    bodyLarge: GoogleFonts.plusJakartaSans(
+        fontSize: 16, fontWeight: FontWeight.normal),
+    bodyMedium: GoogleFonts.plusJakartaSans(
+        fontSize: 14, fontWeight: FontWeight.normal),
+    bodySmall: GoogleFonts.plusJakartaSans(
+        fontSize: 12, fontWeight: FontWeight.normal),
+    labelLarge:
+        GoogleFonts.plusJakartaSans(fontSize: 14, fontWeight: FontWeight.w500),
+    labelMedium:
+        GoogleFonts.plusJakartaSans(fontSize: 12, fontWeight: FontWeight.w500),
+    labelSmall:
+        GoogleFonts.plusJakartaSans(fontSize: 11, fontWeight: FontWeight.w500),
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:test_case_mobile_developer/core/router/app_router.dart';
 import 'package:test_case_mobile_developer/core/theme/app_theme.dart';
+import 'package:test_case_mobile_developer/features/address/presentation/bloc/bloc/address_bloc.dart';
 import 'package:test_case_mobile_developer/features/auth/presentation/bloc/auth/auth_bloc.dart';
 import 'package:test_case_mobile_developer/injections.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,11 +25,14 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => getIt<AuthBloc>(),
         ),
+        BlocProvider(
+          create: (context) => getIt<AddressBloc>(),
+        ),
       ],
       child: MaterialApp.router(
         title: 'Test Case Mobile Developer.',
         theme: ThemeData(
-          textTheme: AppTheme.interTextTheme,
+          textTheme: AppTheme.jakartaSansTextTheme,
           scaffoldBackgroundColor: Colors.white,
         ),
         routeInformationParser: AppRouter.goRouter.routeInformationParser,
